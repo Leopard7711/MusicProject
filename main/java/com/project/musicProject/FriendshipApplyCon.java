ef2ee5a10d2407d391ed8b2a4f2ee8394c5a77c9
Leopard7711 <leopard7711@naver.com>
2023-12-04 00:58:30 +0900

    test2
    test2

--- main/java/com/project/musicProject/FriendshipApplyCon.java
+++ main/java/com/project/musicProject/FriendshipApplyCon.java
@@ -15,8 +15,7 @@ import org.json.JSONObject;
 @WebServlet("/FriendshipApplyCon")
 public class FriendshipApplyCon extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-   
+	//s
 
 	 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 	FriendshipDAO dao = new FriendshipDAO();
